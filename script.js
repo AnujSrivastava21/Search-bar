31a7a0fb1d933a25e788fc1765acfba7bfafaf4d
Anuj Srivastava <87596242+AnujSrivastava21@users.noreply.github.com>
2024-04-18 00:04:10 +0530

    Done

---
+++ script.js
@@ -0,0 +1,5 @@
+ const searchBarContainer = document.querySelector('#container')
+ const magnifier = document.querySelector('.maginfier')
+ magnifier.addEventListener('click',()=>{
+    searchBarContainer.classList.toggle('active')
+ });
